6f9188d1f15851936fc186dec331d57bc86110c8
Aarav Mithrani <aarav.mi33@gmail.com>
2025-11-24 03:05:53 +0530

    connect to mongo

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import { DB_NAME} from "../constants.js";
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+        try{
+            const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+            console.log(`\n Mongo connected !! DB HOST: ${connectionInstance.connection.host}`);
+        } catch (error){
+            console.log("Failed in connecting to MongoDB", error);
+            process.exit(1);
+        }
+}
+
+
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,44 @@
+// require("dotenv").config({path:'./env'})
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({path:'./.env'})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using iife here with try catch and throw for errors
+
+// import express from "express";
+
+// const app = express()
+// ( async() =>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         console.log("Connected to MongoDB")
+//         app.on("error",(error)=>{
+//             console.log("Error in connecting to MongoDB", error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`Server is running on port ${process.env.PORT}`)
+//         })
+//     } catch (error){
+//         console.log("Error", error)
+//         throw error
+//     }
+// })()
